9b485955ae82a1e3786837f2104c2c6e1fda12e0
maclamour <maclamour@gmail.com>
2022-10-11 08:10:09 -0400

    datebase connected, created items, rendering on page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Customer(models.Model):
+    user= models.OneToOneField(User,on_delete=models.CASCADE,null=True, blank=True) 
+    name = models.CharField(max_length=255,null=True)
+    email = models.EmailField(unique=True)
+    
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255,null=True)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 #...
 from django.views.generic.base import TemplateView
+from .models import Product
 
 
 # Create your views here.
@@ -11,11 +12,16 @@ from django.views.generic.base import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 
-class Store:
-    def __init__(self,name,img):
-        self.name = name
-        self.img = img
+# class Store:
+#     def __init__(self,name,img):
+#         self.name = name
+#         self.img = img
 
 
 class StoreList(TemplateView):
     template_name = "store_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.all()
+        return context
