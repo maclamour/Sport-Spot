b62cca560d389aa15d9fb0454146b8e2881bea8a
maclamour <maclamour@gmail.com>
2022-10-11 08:51:23 -0400

    able to make products with django image uploder and have images render properly

--- main_app/models.py
+++ main_app/models.py
@@ -16,9 +16,19 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255,null=True)
+    img = models.ImageField(null=True,blank=True)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     last_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
+    
+    @property
+    def imgUrl(self):
+        try:
+            url=self.img.url
+        except:
+            url=''
+        return url    
+        
