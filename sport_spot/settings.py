db12c9c8ba43865c8acdb5c401e05af2d12a9e4d
maclamour <maclamour@gmail.com>
2022-10-13 10:02:04 -0400

    heroku test

--- sport_spot/settings.py
+++ sport_spot/settings.py
@@ -135,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+import django_heroku
+django_heroku.settings(locals())
