791a303bc5b4839c44439cb21762b5d561be8ecc
maclamour <maclamour@gmail.com>
2022-10-13 10:29:42 -0400

    heroku

--- sport_spot/settings.py
+++ sport_spot/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()  # loads the configs from .env
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 import django_heroku
 django_heroku.settings(locals())
+
