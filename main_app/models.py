e843f249ddef94d6e46f4016e49c23c66b8435c0
maclamour <maclamour@gmail.com>
2022-10-13 08:03:50 -0400

    built out order and orderitem models

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 
 from distutils.command.upload import upload
+from itertools import product
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -34,3 +35,24 @@ class Product(models.Model):
             url=''
         return url    
         
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,blank=True, null=True)
+    order_date = models.DateField(auto_now_add=True)
+    complated_order = models.BooleanField(default=False)
+    order_id = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.id
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = models.IntegerField(default=0, null=True,blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+
+
