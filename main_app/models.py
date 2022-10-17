b44ffcb09e06d8e1d471a1e240a6307548db49ff
maclamour <maclamour@gmail.com>
2022-10-17 08:23:22 -0400

    updating readme

--- main_app/models.py
+++ main_app/models.py
@@ -50,8 +50,8 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True,blank=True)
     date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.product.name
+    # def __str__(self):
+    #     return self.product.name
 
 
 
